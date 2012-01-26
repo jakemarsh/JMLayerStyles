d824f645cd14ee224811a7a598e50864cca6db09
Jake Marsh <jake@thejakemarsh.com>
2012-01-26 08:04:51 -0600

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Jake Marsh on 1/26/12.
+//  Copyright (c) 2012 Rubber Duck Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Jake Marsh on 1/26/12.
+//  Copyright (c) 2012 Rubber Duck Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
