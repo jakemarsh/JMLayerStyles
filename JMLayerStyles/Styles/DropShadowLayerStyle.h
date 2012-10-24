33e730cf560b0201a4c1c77a32c4e07c8997659d
Jake Marsh <jake@thejakemarsh.com>
2012-10-24 16:30:13 -0500

    Cleanup

--- JMLayerStyles/Styles/DropShadowLayerStyle.h
+++ JMLayerStyles/Styles/DropShadowLayerStyle.h
@@ -3,7 +3,7 @@
 //  FacebookAlertView
 //
 //  Created by Jake Marsh on 1/24/12.
-//  Copyright (c) 2012 Rubber Duck Software. All rights reserved.
+//  Copyright (c) 2012 Jake Marsh. All rights reserved.
 //
 
 #import "JMViewLayerStyle.h"
